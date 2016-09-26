3a205697e740522e39e122946feb822f0d9543ba
haochengxu <haochengx@gmail.com>
2016-09-27 01:11:12 +0800

    4*2 second commit

---
+++ js/charts.js
@@ -0,0 +1,72 @@
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('chart'));
+
+  // 指定图表的配置项和数据
+  option = {
+    backgroundColor: '#2c343c',
+
+    title: {
+        text: '内在之花测评',
+        left: 'center',
+        top: 20,
+        textStyle: {
+            color: '#ccc'
+        }
+    },
+
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+
+    visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+            colorLightness: [0, 1]
+        }
+    },
+    series : [
+        {
+            name:'访问来源',
+            type:'pie',
+            radius : '55%',
+            center: ['50%', '50%'],
+            data:[
+                {value:500, name:'成就'},
+                {value:700, name:'身心'},
+                {value:1000, name:'精神'},
+                {value:1000, name:'关系'},
+            ].sort(function (a, b) { return a.value - b.value}),
+            roseType: 'angle',
+            label: {
+                normal: {
+                    textStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    lineStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    },
+                    smooth: 0.2,
+                    length: 10,
+                    length2: 20
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#c23531',
+                    shadowBlur: 200,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+};
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
